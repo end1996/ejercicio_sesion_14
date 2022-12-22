b5a1e49fcfd242d86595ca02b3a05eae21c22306
end1996 <116982425+end1996@users.noreply.github.com>
2022-12-21 19:13:12 -0500

    Cambio en MainActivity.java a Versión 2.0 - 21/12/2022 19:13

--- app/src/main/java/com/example/ejercicio_sesion_14/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_sesion_14/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             if(rs.next()){
 
                 String tipoU=rs.getString(2);
-                Toast.makeText(getApplicationContext(),"Conexion establecida Versión 1.1 "+rs.getString(2),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Conexion establecida Versión 2.0 "+rs.getString(2),Toast.LENGTH_SHORT).show();
                 if(tipoU.compareTo("TU00001")==0) {//cliente
                     Intent x=new Intent(this,frmMainCliente1.class);
                     startActivity(x);
